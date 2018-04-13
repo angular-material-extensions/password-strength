bddfa15fda1181b7a5a0c38b9503965c5ab443ee
anthonynahas <anthony.na@hotmail.de>
2018-04-13 19:14:56 +0200

    fix(ngx-material-password-strength): added the PasswordStrengthComponent to the module

--- src/module/ngx-material-password-strength.module.ts
+++ src/module/ngx-material-password-strength.module.ts
@@ -3,17 +3,19 @@ import {NgModule, ModuleWithProviders} from '@angular/core';
 import {MatProgressBarModule} from '@angular/material';
 
 import {LibComponent} from './component/lib.component';
+import {PasswordStrengthComponent} from './component/password-strength/password-strength.component';
 
 // Export module's public API
 export {LibComponent} from './component/lib.component';
+export {PasswordStrengthComponent} from './component/password-strength/password-strength.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MatProgressBarModule
   ],
-  exports: [LibComponent],
-  declarations: [LibComponent]
+  exports: [LibComponent, PasswordStrengthComponent],
+  declarations: [LibComponent, PasswordStrengthComponent]
 })
 export class NgxMaterialPasswordStrengthModule {
   static forRoot(): ModuleWithProviders {
