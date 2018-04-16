6496491f0ea2ae5f5ea7f0b8105f10b1269bdc87
anthonynahas <anthony.na@hotmail.de>
2018-04-16 22:46:07 +0200

    test(ngx-material-password-strength): added the missed import in the main component spec

--- src/module/component/password-strength/password-strength.component.spec.ts
+++ src/module/component/password-strength/password-strength.component.spec.ts
@@ -1,25 +1,27 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-//
-// import { PasswordStrengthComponent } from './password-strength.component';
-//
-// describe('PasswordStrengthComponent', () => {
-//   let component: PasswordStrengthComponent;
-//   let fixture: ComponentFixture<PasswordStrengthComponent>;
-//
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ PasswordStrengthComponent ]
-//     })
-//     .compileComponents();
-//   }));
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(PasswordStrengthComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {PasswordStrengthComponent} from './password-strength.component';
+import {MatProgressBarModule} from '@angular/material';
+
+describe('PasswordStrengthComponent', () => {
+  let component: PasswordStrengthComponent;
+  let fixture: ComponentFixture<PasswordStrengthComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [MatProgressBarModule],
+      declarations: [PasswordStrengthComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PasswordStrengthComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
