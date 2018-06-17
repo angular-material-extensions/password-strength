aad0b684d78b1130dd7b1a64b035847daf46506c
Anthony Nahas <anthony.na@hotmail.de>
2018-06-17 22:54:15 +0200

    refactor(package): renamed the moduthe info component to `mat-password-strength-info`

--- src/module/component/mat-password-strength-info/mat-password-strength-info.component.spec.ts
+++ src/module/component/mat-password-strength-info/mat-password-strength-info.component.spec.ts
@@ -1,24 +1,24 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import {NgxMaterialPasswordStrengthInfoComponent} from './ngx-material-password-strength-info.component';
+import {MatPasswordStrengthInfoComponent} from './mat-password-strength-info.component';
 import {MatCardModule, MatIconModule} from '@angular/material';
 import {PasswordStrengthComponent} from '../ngx-material-password-strength/password-strength.component';
 
 describe('NgxMaterialPasswordStrengthInfoComponent', () => {
-  let component: NgxMaterialPasswordStrengthInfoComponent;
-  let fixture: ComponentFixture<NgxMaterialPasswordStrengthInfoComponent>;
+  let component: MatPasswordStrengthInfoComponent;
+  let fixture: ComponentFixture<MatPasswordStrengthInfoComponent>;
   const passwordStrengthComponent: PasswordStrengthComponent = new PasswordStrengthComponent();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [MatCardModule, MatIconModule],
-      declarations: [NgxMaterialPasswordStrengthInfoComponent]
+      declarations: [MatPasswordStrengthInfoComponent]
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NgxMaterialPasswordStrengthInfoComponent);
+    fixture = TestBed.createComponent(MatPasswordStrengthInfoComponent);
     component = fixture.componentInstance;
     component.passwordComponent = passwordStrengthComponent;
     fixture.detectChanges();
--- src/module/component/mat-password-strength-info/mat-password-strength-info.component.ts
+++ src/module/component/mat-password-strength-info/mat-password-strength-info.component.ts
@@ -2,11 +2,11 @@ import {Component, Input, OnInit} from '@angular/core';
 import {PasswordStrengthComponent} from '../ngx-material-password-strength/password-strength.component';
 
 @Component({
-  selector: 'ngx-material-password-strength-info',
-  templateUrl: './ngx-material-password-strength-info.component.html',
-  styleUrls: ['./ngx-material-password-strength-info.component.scss']
+  selector: 'mat-password-strength-info',
+  templateUrl: './mat-password-strength-info.component.html',
+  styleUrls: ['./mat-password-strength-info.component.scss']
 })
-export class NgxMaterialPasswordStrengthInfoComponent implements OnInit {
+export class MatPasswordStrengthInfoComponent implements OnInit {
 
   @Input()
   passwordComponent: PasswordStrengthComponent;
