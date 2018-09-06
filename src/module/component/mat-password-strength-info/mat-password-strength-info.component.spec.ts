34d983c99e2b3fb4037bac69ba3b21eb5c28aabe
Anthony Nahas <anthony.na@hotmail.de>
2018-09-06 17:17:16 +0200

    test(package): spec of `mat-password-strength-info` fixed

--- src/module/component/mat-password-strength-info/mat-password-strength-info.component.spec.ts
+++ src/module/component/mat-password-strength-info/mat-password-strength-info.component.spec.ts
@@ -3,18 +3,27 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {MatPasswordStrengthInfoComponent} from './mat-password-strength-info.component';
 import {MatCardModule, MatIconModule} from '@angular/material';
 import {MatPasswordStrengthComponent} from '../mat-password-strength/mat-password-strength.component';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('NgxMaterialPasswordStrengthInfoComponent', () => {
   let component: MatPasswordStrengthInfoComponent;
   let fixture: ComponentFixture<MatPasswordStrengthInfoComponent>;
   const passwordStrengthComponent: MatPasswordStrengthComponent = new MatPasswordStrengthComponent();
 
+  Object.defineProperty(window.document.body.style, 'backface-visibility', {
+    value: () => {
+      return {
+        enumerable: true,
+        configurable: true
+      };
+    },
+  });
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [MatCardModule, MatIconModule],
+      imports: [NoopAnimationsModule, MatCardModule, MatIconModule],
       declarations: [MatPasswordStrengthInfoComponent]
-    })
-      .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
