96c83de8a60b22e9999159878dee43683b63bad1
Anthony Nahas <anthony.na@hotmail.de>
2019-03-09 23:09:30 +0100

    feat(project): added schematics support #145

---
+++ src/schematics/src/ng-add/index.ts
@@ -0,0 +1,91 @@
+import {chain, noop, Rule, SchematicContext, Tree} from '@angular-devkit/schematics';
+import {NodePackageInstallTask} from '@angular-devkit/schematics/tasks';
+import {
+  addModuleImportToRootModule,
+  addPackageJsonDependency,
+  getProjectFromWorkspace,
+  getWorkspace,
+  NodeDependency,
+  NodeDependencyType
+} from 'schematics-utilities';
+import * as fs from 'fs';
+
+const getPackageJsonVersion = () => {
+  // We parse the json file instead of using require because require caches
+  // multiple calls so the version number won't be updated
+  const version = JSON.parse(fs.readFileSync('./../package.json', 'utf8')).version;
+  console.log(`library version used within the schematics ${version}`);
+  return version;
+};
+
+
+// You don't have to export the function as default. You can also have more than one rule factory
+// per file.
+export function addPackageJsonDependencies(): Rule {
+  return (host: Tree, context: SchematicContext) => {
+
+    const ngCoreVersionTag = getPackageVersionFromPackageJson(host, '@angular/core');
+
+    const dependencies: NodeDependency[] = [
+      {type: NodeDependencyType.Default, version: getPackageJsonVersion(), name: '@angular-material-extensions/password-strength'},
+      {type: NodeDependencyType.Default, version: ngCoreVersionTag || '7.2.7', name: '@angular/animations'},
+      {type: NodeDependencyType.Default, version: ngCoreVersionTag || '7.2.7', name: '@angular/forms'}
+    ];
+
+    dependencies.forEach(dependency => {
+      addPackageJsonDependency(host, dependency);
+      context.logger.log('info', `✅️ Added "${dependency.name}" into ${dependency.type}`);
+    });
+
+    return host;
+  };
+}
+
+export function installPackageJsonDependencies(): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    context.addTask(new NodePackageInstallTask());
+    context.logger.log('info', `🔍 Installing packages...`);
+
+    return host;
+  };
+}
+
+export function addModuleToImports(options: any): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    const workspace = getWorkspace(host);
+    const project = getProjectFromWorkspace(
+      workspace,
+      // Takes the first project in case it's not provided by CLI
+      options.project ? options.project : Object.keys(workspace['projects'])[0]
+    );
+    const moduleName = 'MatPasswordStrengthModule';
+
+    addModuleImportToRootModule(host, moduleName, '@angular-material-extensions/password-strength', project);
+    context.logger.log('info', `✅️ "${moduleName}" is imported`);
+
+    return host;
+  };
+}
+
+/** Gets the version of the specified package by looking at the package.json in the given tree. */
+export function getPackageVersionFromPackageJson(tree: Tree, name: string): string | null {
+  if (!tree.exists('package.json')) {
+    return null;
+  }
+
+  const packageJson = JSON.parse(tree.read('package.json')!.toString('utf8'));
+
+  if (packageJson.dependencies && packageJson.dependencies[name]) {
+    return packageJson.dependencies[name];
+  }
+
+  return null;
+}
+
+export default function (options: any): Rule {
+  return chain([
+    options && options.skipPackageJson ? noop() : addPackageJsonDependencies(),
+    options && options.skipPackageJson ? noop() : installPackageJsonDependencies(),
+    options && options.skipModuleImport ? noop() : addModuleToImports(options),
+  ]);
+}
