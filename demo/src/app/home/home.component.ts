ff764e1c22e59ffd9726ac900486ba8f1dd09dda
Anthony Nahas <anthony.na@hotmail.de>
2019-04-18 13:13:35 +0300

    fix(demo): minor integration of the latest changes

--- demo/src/app/home/home.component.ts
+++ demo/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
-import {ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import {Title} from '@angular/platform-browser';
+import {MatPasswordStrengthComponent} from '@angular-material-extensions/password-strength';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +10,14 @@ import {Title} from '@angular/platform-browser';
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('passwordComponentWithValidation')
+  passwordComponentWithValidation2: MatPasswordStrengthComponent;
+
+  formGroup: FormGroup;
+  passwordFormControl: FormControl;
+  x: FormControl;
 
   constructor(private titleService: Title) {
   }
@@ -16,9 +25,11 @@ export class HomeComponent implements OnInit {
   password: string;
   showDetails: boolean;
   showDetails2: boolean;
+  showDetails3: boolean;
 
   viewSource: boolean;
   viewSource2: boolean;
+  viewSource3: boolean;
   color = '';
 
   ts = `import {Component, OnInit, ViewEncapsulation} from '@angular/core';
@@ -139,11 +150,46 @@ export class HomeComponent implements OnInit {
       </mat-card-content>`;
 
   ngOnInit() {
+    console.log('home on init');
     this.titleService.setTitle('Home | mat-password-strength');
+    // this.passwordFormControl = this.passwordComponentWithValidation2.passwordFormControl;
+    // this.passwordFormControl = new FormControl('', this.passwordComponentWithValidation2.Validators);
+    // console.log('passwordFormControl form control = ', this.passwordFormControl);
+
+    this.passwordComponentWithValidation2.passwordFormControl.valueChanges.subscribe(() => {
+      console.log('passwordFormControl form control = ', this.passwordComponentWithValidation2.passwordFormControl);
+    });
+
+    // this.passwordFormControl.valueChanges.subscribe(value => {
+    //   console.log('passwordFormControl form control = ', this.passwordFormControl);
+    // });
+
+    this.formGroup = new FormGroup({
+      'password': this.passwordComponentWithValidation2.passwordFormControl
+    });
   }
 
   onStrengthChanged(strength: number) {
-    console.log('password strength = ', strength);
+    // console.log('password strength = ', strength);
   }
+
+  ngAfterViewInit(): void {
+    // console.log('pass comp on ngAfterViewInit', this.passwordComponentWithValidation2);
+  }
+
+  // validate(): ValidatorFn {
+  //   const validator = (control: AbstractControl): { [key: string]: any } => {
+  //     // this.isUndefinedOrEmpty(control);
+  //     console.log('validate', !RegExpValidator.lowerCase.test(control.value));
+  //     // control.setErrors({test: true});
+  //     if (!RegExpValidator.lowerCase.test(control.value)) {
+  //       return {
+  //         'lowerCase': true
+  //       };
+  //     }
+  //     return undefined;
+  //   };
+  //   return validator;
+  // }
 
 }
--- demo/src/app/home/home.module.ts
+++ demo/src/app/home/home.module.ts
@@ -4,12 +4,14 @@ import {CommonModule} from '@angular/common';
 import {HomeRoutingModule} from './home-routing.module';
 import {HomeComponent} from './home.component';
 import {AppSharedModule} from '../shared';
+import {MatPasswordStrengthModule} from '@angular-material-extensions/password-strength';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule,
-    AppSharedModule
+    AppSharedModule,
+    MatPasswordStrengthModule
   ],
   declarations: [HomeComponent],
 })
