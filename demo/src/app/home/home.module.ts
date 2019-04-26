11662393fcac425599abf4d5ba3b0e043a595f85
Anthony Nahas <anthony.na@hotmail.de>
2019-04-26 19:13:26 +0300

    feat(demo): integration of angular material extensions jumbotron lib #165

--- demo/src/app/home/home.module.ts
+++ demo/src/app/home/home.module.ts
@@ -6,6 +6,7 @@ import {HomeComponent} from './home.component';
 import {AppSharedModule} from '../shared';
 import {MatPasswordStrengthModule} from '@angular-material-extensions/password-strength';
 import {MarkdownModule} from 'ngx-markdown';
+import {MatJumbotronModule} from '@angular-material-extensions/jumbotron';
 
 @NgModule({
   imports: [
@@ -13,6 +14,7 @@ import {MarkdownModule} from 'ngx-markdown';
     HomeRoutingModule,
     AppSharedModule,
     MarkdownModule.forChild(),
+    MatJumbotronModule,
     MatPasswordStrengthModule
   ],
   declarations: [HomeComponent],
