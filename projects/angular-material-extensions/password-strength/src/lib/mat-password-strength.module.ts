dbcac406880f4a9333314375a48d19bfaf25a4bd
Anthony Nahas <anthony.na@hotmail.de>
2020-02-08 22:51:08 +0100

    fix(lib): upgraded the library to angular v9

---
+++ projects/angular-material-extensions/password-strength/src/lib/mat-password-strength.module.ts
@@ -0,0 +1,50 @@
+import {CommonModule} from '@angular/common';
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatRippleModule} from '@angular/material/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+
+import {MatPasswordStrengthComponent} from './component/mat-password-strength/mat-password-strength.component';
+import {MatPasswordStrengthInfoComponent} from './component/mat-password-strength-info/mat-password-strength-info.component';
+import {MatPassToggleVisibilityComponent} from './component/mat-pass-toggle-visibility/mat-pass-toggle-visibility.component';
+
+
+// Export module's public API
+export {MatPasswordStrengthComponent} from './component/mat-password-strength/mat-password-strength.component';
+export {
+  MatPasswordStrengthInfoComponent
+} from './component/mat-password-strength-info/mat-password-strength-info.component';
+export {MatPassToggleVisibilityComponent} from './component/mat-pass-toggle-visibility/mat-pass-toggle-visibility.component';
+export {MatPasswordStrengthValidator} from './validator/mat-password-strength-validator';
+// validator
+export {RegExpValidator} from './validator/regexp.class';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatProgressBarModule,
+    MatCardModule,
+    MatIconModule,
+    MatRippleModule
+  ],
+  exports: [
+    MatPasswordStrengthComponent,
+    MatPasswordStrengthInfoComponent,
+    MatPassToggleVisibilityComponent
+  ],
+  declarations: [
+    MatPasswordStrengthComponent,
+    MatPasswordStrengthInfoComponent,
+    MatPassToggleVisibilityComponent
+  ],
+  entryComponents: [MatPassToggleVisibilityComponent]
+})
+export class MatPasswordStrengthModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: MatPasswordStrengthModule,
+      providers: []
+    };
+  }
+}
