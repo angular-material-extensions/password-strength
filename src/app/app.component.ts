2e7ca661792f64f47315796c8a859f1860ef50eb
Anthony Nahas <anthony.na@hotmail.de>
2020-02-09 21:32:44 +0100

    fix(demo): improved the demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPasswordStrengthComponent} from '@angular-material-extensions/password-strength';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  @ViewChild('passwordComponentWithConfirmation', {static: true})
+  passwordComponentWithConfirmation: MatPasswordStrengthComponent;
+
   title = 'password-strength';
+
+  password: string;
+  showDetails: boolean;
+  showDetails2: boolean;
+  showDetails3: boolean;
+
+  viewSource: boolean;
+  viewSource2: boolean;
+  viewSource3: boolean;
+  color = '';
+
+  ngOnInit() {
+    console.log('home on init');
+
+    this.passwordComponentWithConfirmation.passwordFormControl.valueChanges.subscribe(() => {
+      console.log('passwordFormControl form control = ', this.passwordComponentWithConfirmation.passwordFormControl);
+    });
+
+    this.passwordComponentWithConfirmation.passwordConfirmationFormControl.valueChanges.subscribe(() => {
+      console.log('passwordFormControl form control = ', this.passwordComponentWithConfirmation.passwordConfirmationFormControl);
+    });
+
+  }
+
+  onStrengthChanged(strength: number) {
+    // console.log('password strength = ', strength);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatPasswordStrengthModule} from '@angular-material-extensions/password-strength';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MarkdownModule} from 'ngx-markdown';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule.withServerTransition({appId: 'serverApp'}),
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    MarkdownModule.forRoot({loader: HttpClient}),
+    MatPasswordStrengthModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatIconModule,
     MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
