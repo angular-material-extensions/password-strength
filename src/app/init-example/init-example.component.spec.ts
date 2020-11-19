27f9978edd6911ac2350a366c9c820e72a1c0eef
Anthony Nahas <anthony.na@hotmail.de>
2020-11-20 00:27:51 +0100

    fix(package): upgraded angular to v11

--- src/app/init-example/init-example.component.spec.ts
+++ src/app/init-example/init-example.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { InitExampleComponent } from './init-example.component';
 
@@ -6,7 +6,7 @@ describe('InitExampleComponent', () => {
   let component: InitExampleComponent;
   let fixture: ComponentFixture<InitExampleComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ InitExampleComponent ]
     })
