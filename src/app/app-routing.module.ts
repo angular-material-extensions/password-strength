67425855f2ad644387e0634204aee4006b10f2d4
Anthony Nahas <anthony.na@hotmail.de>
2022-07-05 20:31:44 +0200

    fix(demo): minor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabled',
+    initialNavigation: 'enabledNonBlocking',
     relativeLinkResolution: 'legacy'
 })],
   exports: [RouterModule]
