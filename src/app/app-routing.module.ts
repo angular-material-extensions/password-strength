4729ebd9e84b32a85dafd59d928e8c3d441558aa
Anthony Nahas <anthony.na@hotmail.de>
2022-11-25 16:28:49 +0100

    fix(project): upgraded angular cli and core to v15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,8 +17,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabledNonBlocking',
-    relativeLinkResolution: 'legacy'
+    initialNavigation: 'enabledNonBlocking'
 })],
   exports: [RouterModule]
 })
