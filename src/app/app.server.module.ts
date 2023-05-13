f35716100afdd18f963362b2c698fa75f6d09bdb
Yberion <brandon.largeau@gmail.com>
2023-05-13 20:15:17 +0200

    fix: add FlexLayoutServerModule on server module

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-
-import { AppModule } from './app.module';
+import { FlexLayoutServerModule } from '@ngbracket/ngx-layout/server';
 import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+    FlexLayoutServerModule
   ],
   bootstrap: [AppComponent],
 })
